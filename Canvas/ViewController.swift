26a336abccdfb86524a7f84c3b17437de02daa70
efwong <edwinfwong@gmail.com>
2016-11-04 22:45:28 -0700

    Implemented view panning

--- Canvas/ViewController.swift
+++ Canvas/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trayView: UIView!
+    var trayOriginalCenter: CGPoint!
+    var trayCenterWhenOpen: CGPoint!
+    var trayCenterWhenClosed: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        trayCenterWhenOpen = CGPoint(x: trayView.center.x, y: 806-170)
+        trayCenterWhenClosed = CGPoint(x: trayView.center.x, y: 806)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTrayPanGesture(_ panGestureRecognizer: UIPanGestureRecognizer) {
+        let point = panGestureRecognizer.location(in: trayView)
+        //let translation = panGestureRecognizer.translation(in: trayView)
+        let velocity = panGestureRecognizer.velocity(in: trayView)
+        if panGestureRecognizer.state == .began {
+            trayOriginalCenter = trayView.center
+        } else if panGestureRecognizer.state == .changed {
+            if velocity.y > 0{
+            UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
+                    self.trayView.center.y = self.trayCenterWhenClosed.y
+                }, completion: nil)
+            }else if velocity.y < 0{
+                UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseIn, animations: {
+                    self.trayView.center.y = self.trayCenterWhenOpen.y
+                    }, completion: nil)
+            }
+            //trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+        } else if panGestureRecognizer.state == .ended {
+            print("Gesture ended at: \(point)")
+        }
+    }
 
 }
 
